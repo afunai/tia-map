cb4843a27c62ef3f96224e17702c5d24a1d339e5
Akira FUNAI <funai@mac-mini.local>
2017-08-08 20:48:47 +0900

    initialize omniauth with ENV['CONSUMER_KEY'] and ENV['CONSUMER_SECRET']

--- app.rb
+++ app.rb
@@ -2,6 +2,10 @@ require 'sinatra'
 require 'omniauth-twitter'
 require 'json'
 
+use OmniAuth::Builder do
+  provider :twitter, ENV['CONSUMER_KEY'], ENV['CONSUMER_SECRET']
+end
+
 get '/' do
   '<a href="/auth/twitter">sign in with Twitter</a>'
 end
